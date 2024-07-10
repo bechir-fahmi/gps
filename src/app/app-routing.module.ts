984f2317715bad93cbbd632228cd5959ad8a1f03
bachirfahmi <bachir.fahmi@dyno-motiva.com>
2024-07-10 12:11:31 +0100

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MapComponent } from './routes/map/map.component';
 import { DeviceListComponent } from './routes/device-list/device-list.component';
 import { LoginComponent } from './routes/login/login.component';
 import { DeviceCardComponent } from './routes/device-card/device-card.component';
-
+import { authGuard } from './core/authGuard/auth.guard';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'map', component: MapComponent },
-  { path: 'device-list', component: DeviceListComponent },
-  { path: 'device-card', component: DeviceCardComponent },
+  { path: 'map', component: MapComponent, canActivate: [authGuard] },
+  { path: 'device-list', component: DeviceListComponent, canActivate: [authGuard] },
+  { path: 'device-card', component: DeviceCardComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: '/map', pathMatch: 'full' }, // Redirect to /map by default
+  { path: '**', redirectTo: '/map' } // Redirect unknown routes to /map
 ];
 
 @NgModule({
