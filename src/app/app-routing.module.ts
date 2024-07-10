c92db78b88816b00f272d439f6c84dc12b2bb3bf
bachirfahmi <bachir.fahmi@dyno-motiva.com>
2024-07-10 13:01:41 +0100

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { DeviceListComponent } from './routes/device-list/device-list.component'
 import { LoginComponent } from './routes/login/login.component';
 import { DeviceCardComponent } from './routes/device-card/device-card.component';
 import { authGuard } from './core/authGuard/auth.guard';
+import { redirectGuard } from './core/redirectGuard/redirect.guard';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'map', component: MapComponent, canActivate: [authGuard] },
   { path: 'device-list', component: DeviceListComponent, canActivate: [authGuard] },
   { path: 'device-card', component: DeviceCardComponent, canActivate: [authGuard] },
-  { path: '', redirectTo: '/map', pathMatch: 'full' }, // Redirect to /map by default
-  { path: '**', redirectTo: '/map' } // Redirect unknown routes to /map
+  // { path: '', canActivate: [redirectGuard], pathMatch: 'full' },
+  { path: '**', redirectTo: 'map', pathMatch: 'full' } // Redirect all other paths
 ];
 
 @NgModule({
--- src/app/core/authGuard/auth.guard.ts
+++ src/app/core/authGuard/auth.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { LoginService } from '../../Services/login/login.service'
+import { LoginService } from '../../Services/login/login.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(LoginService);
