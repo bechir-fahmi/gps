624e67f28e74a84118d5f0a0e601f5616e6d3a4c
bachirfahmi <bachir.fahmi@dyno-motiva.com>
2024-07-10 13:01:52 +0100

    add redirect guard

---
+++ src/app/core/redirectGuard/redirect.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { LoginService } from '../../Services/login/login.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class redirectGuard implements CanActivate {
+  constructor(private authService: LoginService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/map']);
+      return false;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
